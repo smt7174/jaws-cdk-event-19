149d77bd33127fc64020711ed4b850ec49c761ca
smt7174 <suzukima@belltreesoft.jp>
2025-02-09 09:08:20 +0900

    Initial commit

---
+++ lib/jaws-cdk-event-19-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class JawsCdkEvent19Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'JawsCdkEvent19Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
