f825d92e53ec466e0b814cf3314f8003c3636a98
smt7174 <suzukima@belltreesoft.jp>
2025-02-10 10:40:40 +0900

    import分などを少し変更

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,9 +1,10 @@
 // typeを付けないとうまく動かない
 import type { LambdaFunctionURLEvent, LambdaFunctionURLResult } from 'aws-lambda/trigger/lambda-function-url'
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb/dist-types/DynamoDBClient";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb/dist-types/DynamoDBDocumentClient";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb/dist-types/commands/ScanCommand";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 
+const TABLE_NAME_DEFAULT = 'tower-of-druaga'
 const TABLE_REGION_DEFAULT = 'ap-northeast-1'
 
 const handler = async function (event: LambdaFunctionURLEvent) {
@@ -16,7 +17,7 @@ const handler = async function (event: LambdaFunctionURLEvent) {
   const docClient = DynamoDBDocumentClient.from(client);
   
   const command = new ScanCommand({
-    TableName: process.env.TABLE_NAME,
+    TableName: process.env.TABLE_NAME || TABLE_NAME_DEFAULT,
   });
 
   const output = await docClient.send(command);
