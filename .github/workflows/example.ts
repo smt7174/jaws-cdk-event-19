7e75e155fdc48aa9d7cc562c93a88e1f53f93a82
smt7174 <suzukima@belltreesoft.jp>
2025-04-29 22:02:48 +0900

    ghats 関連のファイルを追加

---
+++ .github/workflows/example.ts
@@ -0,0 +1,24 @@
+// 参考：https://zenn.dev/kou_pg_0131/articles/ghats-introduction
+//      https://docs.github.com/ja/actions/writing-workflows/quickstart
+
+// ghats から Workflow, Job を import
+import { Workflow, Job } from "ghats";
+
+// workflow を定義
+const workflow = new Workflow("Hello", {
+  on: "push",
+});
+
+// job を定義
+workflow.addJob(
+  new Job("hello", {
+    runsOn: "ubuntu-latest",
+  })
+    .uses("actions/checkout@v4", {
+      with: { "persist-credentials": "false" }
+    })
+    .run("echo 'Hello, world!'"),
+);
+
+// workflow を default export
+export default workflow;
